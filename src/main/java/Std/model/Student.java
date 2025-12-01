193898046d4ce8c41957c9d68d4e49a45529b13d
Erasyl <lg.8k.ltre@gmail.com>
2025-12-01 10:38:21 +0500

    с

--- src/main/java/Std/model/Student.java
+++ src/main/java/Std/model/Student.java
@@ -15,13 +15,28 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
-    private String lastName;
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
     private int age;
     private String email;
 
-    @OneToMany(fetch = FetchType.EAGER)
-    private List <Group> groups;
     @ManyToMany(fetch = FetchType.EAGER)
-    private List <Lecture> lectures;
+    @JoinTable(
+            name = "student_groups",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "groups_id")
+    )
+    private List<Group> groups;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "student_lectures",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "lectures_id")
+    )
+    private List<Lecture> lectures;
 }
+
